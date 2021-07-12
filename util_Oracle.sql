6f4e2d7ccab24532e15b9ecbbc69244bb751521a
Igor Becker <igor.becker@smlbrasil.com.br>
2021-07-12 08:20:23 -0300

    Atualizando arquivos

--- util_Oracle.sql
+++ util_Oracle.sql
@@ -25,7 +25,7 @@ INTO V_CODTASK, V_CODFLOWELEMENT
 FROM wfTASK T1
 WHERE 
 ROWNUM = 1 
-AND T1.CodFlow = (SELECT T2.CodFlow FROM WFFLOW T2 WHERE DsFlowName = 'UNIMED POA - Contratos Assistenciais e Complementares PF e PME')
+AND T1.CodFlow = (SELECT T2.CodFlow FROM WFFLOW T2 WHERE DsFlowName = 'Nome Do Processo')
 AND T1.DsTaskTitle = 'Nome da Tafefa';
 --SELECT CodFlowElement INTO V_CODFLOWELEMENT FROM wfTASK WHERE codtask = V_CODTASK;
 --V_CODTASK := 1344;
@@ -91,9 +91,9 @@ BEGIN
   --FAZ UM SELECT APENAS FONTES DE DADOS DO TIPO WSDL.
   FOR Fonte IN (SELECT * FROM WFGROUP_VALUE WHERE DsType = 'wsdl') LOOP
   
-            --VERIFICA SE EXISTE O PREFIXO "http://perth01-dev:88" NA URL DA FONTE DE DADOS E SALVA O RESULTADO NA VARIAVEL "CheckExists".
+            --VERIFICA SE EXISTE O PREFIXO "http://enderecodev.com" NA URL DA FONTE DE DADOS E SALVA O RESULTADO NA VARIAVEL "CheckExists".
             BEGIN
-            SELECT INSTR(DsFileName, 'http://perth01-hml:88', 1)
+            SELECT INSTR(DsFileName, 'http://enderecodev.com', 1)
             into CheckExists 
             FROM WFGROUP_VALUE 
             WHERE 
@@ -101,15 +101,15 @@ BEGIN
             
             --SE "CheckExists" > 0 = Existe.
             IF CheckExists > 0 THEN  
-                 --FAZ UM SELECT REMOVENDO O PREFIXO "http://perth01-hml:88" do DsFileName.
-                 SELECT REPLACE(DsFileName, 'http://perth01-hml:88', 'https://orquestra.unimedpoa.com.br')
+                 --FAZ UM SELECT REMOVENDO O PREFIXO "http://enderecoantigo.com" do DsFileName.
+                 SELECT REPLACE(DsFileName, 'http://enderecoantigo.com', 'https://endereconovo.com')
                  into NewDsFileName
                  FROM WFGROUP_VALUE
                  WHERE
                  CodGroupValue = Fonte.CodGroupValue; 
                  
-                 --FAZ UM SELECT REMOVENDO O PREFIXO "http://perth01-hml:88" do DsLocation.
-                 SELECT REPLACE(DsLocation, 'http://perth01-hml:88', 'https://orquestra.unimedpoa.com.br')
+                 --FAZ UM SELECT REMOVENDO O PREFIXO "http://enderecoantigo.com" do DsLocation.
+                 SELECT REPLACE(DsLocation, 'http://enderecoantigo.com', 'https://endereconovo.com')
                  into NewDsLocation
                  FROM WFGROUP_VALUE
                  WHERE
